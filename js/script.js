a798e9f421a880b771d6b476b22456955b11f70a
IT042301 <it042301@IT042301s-MacBook-Pro.local>
2023-07-11 14:07:05 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const heroLeftSlider = document.getElementById("hero-left-slider");
+const heroRightSlider = document.getElementById("hero-right-slider");
+
+heroLeftSlider.addEventListener("click", ()=>{
+    alert();
+})
+
+heroRightSlider.addEventListener("click", ()=>{
+    alert();
+})
